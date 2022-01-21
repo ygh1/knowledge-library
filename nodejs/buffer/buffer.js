845cb7fbe01fa8cf780b6e63a4f167cb7603bfee
yanguanghao <yanguanghao@qihangxingchen.com>
2022-01-21 11:56:30 +0800

    buffer

---
+++ nodejs/buffer/buffer.js
@@ -0,0 +1,4 @@
+// buffer 缓冲区
+const buffer = require('buffer')
+
+console.log(Buffer === buffer.Buffer)
--- nodejs/path/path.js
+++ nodejs/path/path.js
@@ -1,6 +1,17 @@
 const path = require('path')
 
-// 获取basename
-const basename = path.basename('/user/sdfj/index.html');
+// 获取basename 
+// index.html
+// const basename = path.basename('/user/sdfj/index.html')
+// console.log(basename)
 
-console.log(basename)
+
+// .html
+// const extname = path.extname('index.html');
+// console.log(extname)
+
+// 由.或者.. 转换为通过的路径
+// const normalPath = path.normalize('/foo/bar//baz/asdf/quux/.')
+// console.log(normalPath)
+
+
