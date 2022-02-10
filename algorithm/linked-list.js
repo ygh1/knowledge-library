7c79c6c772dc3b9af94f1e4febffd9e9249602b7
yanguanghao <yanguanghao@qihangxingchen.com>
2022-02-10 23:13:03 +0800

    实现一个链表

---
+++ algorithm/linked-list.js
@@ -0,0 +1,125 @@
+var MyLinkedList = function() {
+    this.head = null
+  };
+  
+  /*
+  @param {number} index
+  @return {number}
+  */
+  MyLinkedList.prototype.get = function(index) {
+    let current = this.head
+    let i = 0
+    while(current) {
+        if (index === i++) {
+            return current.value
+        }
+        current = current.next
+    }
+    return -1
+  };
+  
+  /*
+  @param {number} val
+  @return {void}
+  */
+  MyLinkedList.prototype.addAtHead = function(val) {
+    const head = {
+        value: val,
+        next: this.head
+    }
+    if (!this.head) {
+        this.head = head
+    } else {
+        this.head = head    
+    }
+  };
+  
+  /*
+  @param {number} val
+  @return {void}
+  */
+  MyLinkedList.prototype.addAtTail = function(val) {
+    let current = this.head
+    const last = {
+        value: val,
+        next: null
+    }
+    if (!current) {
+        this.head = last
+        return
+    }
+    while(current.next) {
+        current = current.next
+    }
+    current.next = last
+  };
+  
+  /*
+  @param {number} index 
+  @param {number} val
+  @return {void}
+  */
+  MyLinkedList.prototype.addAtIndex = function(index, val) {
+    debugger
+    if (index <= 0) {
+        this.addAtHead(val)
+        return
+    }
+    let current = this.head
+    let i = 0
+    while(current && current.next) {
+        if ((index - 1) === i++) {
+            let next = current.next
+  
+            current.next = {
+                value: val,
+                next
+            }
+            return
+        }
+        current = current.next
+    }
+    if (index - 1 === i) {
+        this.addAtTail(val)
+    }
+  };
+  
+  /*
+  @param {number} index
+  @return {void}
+  */
+  MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if (index === 0) {
+        this.head = this.head.next
+        return
+    }
+    let current = this.head
+    let i = 0
+    let secendLast = null
+    while (current && current.next) {
+        if ((index - 1) === i++) {
+            current.next = {
+                value: current.next.next?.value,
+                next: current.next.next
+            }
+            return
+        }
+        if (current && current.next && !current.next.next) {
+            secendLast = current
+        }
+        current = current.next
+    }
+    if (index - 1 === i) {
+        secendLast.next = {
+            value: val,
+            next: null
+        }
+    }
+  };
+
+  const obj = new MyLinkedList(null)
+  obj.addAtHead(5)
+  obj.addAtIndex(1, 2)
+  obj.get(1)
+  console.log( obj.head )
+  
