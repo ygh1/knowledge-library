81094bd3bd80d604c3f4fbb04ecf20c03e4759ec
yanguanghao <yanguanghao@qihangxingchen.com>
2022-02-17 11:15:58 +0800

    反转链表

---
+++ algorithm/linkedlist/reverse-linkedlist.js
@@ -0,0 +1,34 @@
+const reverseList = head => {
+  if (head === null) {
+    return head
+  }
+  let curr = null
+  while (head) {
+    
+
+    // 保存下一个要循环的节点
+    const next = head.next
+    // 反转
+    head.next = curr
+
+    // 保存当
+    curr = head
+
+    // 循环
+    head = next
+  }
+  return curr
+}
+
+const linedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: null
+    }
+  }
+}
+const reverse = reverseList(linedList)
+console.log(reverse)
